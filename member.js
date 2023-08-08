6f97559150dfe9bb1b818ca2a3af338b2c81a6d1
Tanmay sain <110602436+tanmayysain@users.noreply.github.com>
2023-08-08 06:55:14 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,7 +1,8 @@
 function skillfunction() {
     var skill = document.getElementById("skill");
+    var skillsMember = document.getElementById("skillsmember");
     var skillvalue = skill.options[skill.selectedIndex].value;
     var skilltext = skill.options[skill.selectedIndex].text;
-    document.getElementById("skilltext").innerHTML = skilltext;
-    document.getElementById("skillvalue").innerHTML = skillvalue;
+    document.getElementById("skillvalue").value = skillvalue;
+    document.getElementById("skilltext").value = skilltext;
 }
